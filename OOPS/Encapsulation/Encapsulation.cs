0243c1869dbd375e46908d982cbed84976370f85
[Abbaskalith] <[abkalith@gmail.com]>
2021-10-10 00:44:21 +0530

    OOPS Concept Done

--- OOPS/Encapsulation/Encapsulation.cs
+++ OOPS/Encapsulation/Encapsulation.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOPS.Encapsulation
+namespace OOPSenc.OOPS
 {
-    class Encap
+    class Encapsulation
     {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string age;
+
+        public string Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
     }
 }
